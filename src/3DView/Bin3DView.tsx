9787d5d0a881c194bd9717246342a41bdebd5a5b
Joel Chia <brunchcycleturbojet@gmail.com>
2026-03-24 14:40:51 +0800

    Basic visuals brushup
    - Mirrored render of boxes as reflections
    - Grid fade
    - Simple colours

--- src/3DView/Bin3DView.tsx
+++ src/3DView/Bin3DView.tsx
@@ -1,10 +1,10 @@
 import "../style/Bin3DView.css";
 import { useState, useRef, useLayoutEffect, useEffect } from 'react'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Grid, PerspectiveCamera } from '@react-three/drei'
+import { DoubleSide, EdgesGeometry, LineSegments, LineBasicMaterial, Mesh } from "three";
 
-import { DoubleSide, EdgesGeometry, LineSegments, LineBasicMaterial, Mesh, Vector3, MOUSE } from "three";
-import {Bin, Item} from "../BinData";
+import { Bin, Item } from "../BinData";
 import { CameraControls } from "./CameraControls";
 
 interface Bin3DViewProps {
@@ -26,7 +26,7 @@ function Bin3DView({ bin, items }: Bin3DViewProps) {
     return (
       <mesh position={[bin.width/2, bin.height/2, bin.depth/2]}>
         <boxGeometry args={[bin.width + lineOffset, bin.height + lineOffset, bin.depth + lineOffset]} />
-        <meshBasicMaterial color="lightgray" wireframe={true} transparent opacity={0.25} side={DoubleSide} />
+        <meshBasicMaterial color="rgb(97, 97, 97)" wireframe={true} transparent opacity={0.25} side={DoubleSide} />
       </mesh>
     );
   }
@@ -44,34 +44,41 @@ function Bin3DView({ bin, items }: Bin3DViewProps) {
   }
 
   return (
-    <Canvas id="bin3DView" flat shadows color="rgb(200,200,200)" >
+    <Canvas id="bin3DView" flat shadows >
+      <color attach="background" args={['rgb(235, 232, 232)']} />
 
       <PerspectiveCamera makeDefault fov={30} position={[10, 10, 10]} zoom={1}/>
       <CameraControls bin={bin} />
 
-      <directionalLight position={[5,10,-2]} />
-      <ambientLight intensity={0.3} />
+      <directionalLight position={[5,10,-2]} intensity={1.0}/>
+      <ambientLight intensity={3.0} />
 
       {renderBin()}
+      {renderUnpackedBoxes()}
+
       <Grid 
         renderOrder={-1} 
         position={gridOrigin} 
         infiniteGrid 
         cellSize={0.6} 
-        cellColor={"rgb(200,200,200)"}
+        cellColor={"rgb(68, 68, 68)"}
         cellThickness={0.6} 
         sectionSize={3.3} 
         sectionThickness={1.5} 
-        sectionColor={"rgb(255, 195, 85)"} 
-        fadeFrom={0.3}
-        fadeDistance={8}
-        fadeStrength={1}
+        sectionColor={"rgb(253, 169, 12)"} 
+        fadeFrom={0} // 0: fade from origin, 1: fade from camera
+        fadeDistance={60}
+        fadeStrength={8}
         side={DoubleSide}
-        />
+      />
 
-      {renderUnpackedBoxes()}
-
-      {/* <Environment background preset="sunset" blur={0.8} /> */}
+      {/* Fake reflection (hidden when camera is below floor) */}
+      <FloorMask>
+        <group scale={[1, -1, 1]} position={[0, -0.02, 0]}>
+          {renderBin()}
+          {renderUnpackedBoxes()}
+        </group>
+      </FloorMask>
     </Canvas>
   );
 }
@@ -86,14 +93,14 @@ function ItemBox({ item }: { item: Item }) {
     if (meshRef.current) {
       const geometry = meshRef.current.geometry;
       const edges = new EdgesGeometry(geometry);
-      const wireframe = new LineSegments(edges, new LineBasicMaterial({ color: 0x000000, linewidth: 2 }));
+      const wireframe = new LineSegments(edges, new LineBasicMaterial({ color: "rgb(119, 61, 61)", linewidth: 2 }));
       meshRef.current.add(wireframe);
     }
   }, []);
 
   // Define the distance move objects 'up', so they don't clip with the floor.
   // Use to put 'things' on the floor, example: Ypos = Y scale/2 + offset
-  const YOffsetFromFloor = 0.01;
+  const YOffsetFromFloor = 0.001;
   return (
     <mesh 
       ref={meshRef}
@@ -101,7 +108,25 @@ function ItemBox({ item }: { item: Item }) {
       position={[item.x + item.width / 2, item.y + item.height / 2 + YOffsetFromFloor, item.z + item.depth / 2]}
     >
       <boxGeometry args={[item.width, item.height, item.depth]} />
-      <meshStandardMaterial color="orange" />
+      <meshStandardMaterial color="rgb(231, 136, 58)" />
     </mesh>
+  );
+}
+
+// Component to control rendering of children, only when above the floor (xz plane)
+function FloorMask({ children }: { children: React.ReactNode }) {
+  const { camera } = useThree();
+  const [visible, setVisible] = useState(true);
+
+  useFrame(() => {
+    const shouldShow = camera.position.y >= 0;
+    if (visible !== shouldShow) {
+      setVisible(shouldShow);
+    }
+  });
+
+  if (!visible) return null;
+  return (
+    <>{children}</>
   );
 }
